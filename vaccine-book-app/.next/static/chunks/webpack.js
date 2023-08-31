81c6b5ee279f7385871770704facc7ef5e238a5c
Rata Surithpinyo <rata@Ratas-MacBook-Air.local>
2023-08-31 14:14:08 +0700

    assignment#4

--- vaccine-book-app/.next/static/chunks/webpack.js
+++ vaccine-book-app/.next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "431df4d88d6ebd4c"; }
+/******/ 		__webpack_require__.h = function() { return "cff976a206151cee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- vaccine-book-app/src/app/booking/page.tsx
+++ vaccine-book-app/src/app/booking/page.tsx
@@ -1,7 +1,7 @@
 export default function Booking() {
   return (
     <main>
-      <div className="mt-10 text-center underline decoration-sky-500">
+      <div className="mt-20 text-center underline decoration-sky-500">
         Booking Page
       </div>
     </main>
