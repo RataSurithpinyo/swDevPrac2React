f71d6c88b16bf8bef2a77362a39ae1456bfbeaf7
RataSurithpinyo <rataprim30026@gmail.com>
2023-11-16 15:17:04 +0700

    assignment#14

--- vaccine-book-app/cypress/e2e/Hospitals.cy.ts
+++ vaccine-book-app/cypress/e2e/Hospitals.cy.ts
@@ -1,10 +1,12 @@
-describe("Home Page", () => {
+describe("VdeoPlayer & Count Hospitals", () => {
   it("Check VdoPlayer and count hospital(s)", () => {
     cy.visit("/");
-    cy.get("#video").get("#video-button").contains("Pause"); // find('Get your vaccine today.')
+    // cy.get("#video").get("#video-button").contains("Pause"); // find('Get your vaccine today.')
+    cy.get("video");
+    cy.get("video").should("have.prop", "paused", false);
     cy.wait(2000);
     cy.get("#video-button").click();
-    cy.get("#video").get("#video-button").contains("Play");
+    cy.get("video").should("have.prop", "paused", true); //.get("#video-button").contains("Play");
     cy.get("#select-hospital-button").click();
     cy.wait(5000);
     cy.get("#hospital-div").find("img").should("have.length", 3);
